e4b3244877ba4add9e1393e58d38b732a0ec30eb
Dominik Wycisło <d.wycislo@outlook.com>
2018-09-16 01:22:26 +0200

    bug fixes in Book model

--- EBookLibraryData/Migrations/20180915232127_ChangedBookModel.Designer.cs
+++ EBookLibraryData/Migrations/20180915232127_ChangedBookModel.Designer.cs
@@ -4,14 +4,16 @@ using EBookLibraryData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EBookLibraryData.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20180915232127_ChangedBookModel")]
+    partial class ChangedBookModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EBookLibraryData/Migrations/20180915232127_ChangedBookModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EBookLibraryData.Migrations
+{
+    public partial class ChangedBookModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookCoverPath",
+                table: "Books");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BookCoverPath",
+                table: "Books",
+                nullable: true);
+        }
+    }
+}
